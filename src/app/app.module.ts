d5e7bbbb77da2147a9a3be1444e3dd8207dff535
fomoro <fomoro@gmail.com>
2022-12-19 18:27:36 -0500

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './components/app/app.component';
 import { PruebaComponent } from './components/prueba/prueba.component';
 
-
 @NgModule({
-  declarations: [
-<<<<<<< HEAD
-    AppComponent,
-    PruebaComponent
-=======
-    AppComponent
->>>>>>> dc0efef4fa05cf3b1a9302fb15d7b0261577063e
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PruebaComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
